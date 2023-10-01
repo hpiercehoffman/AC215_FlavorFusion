6d987afdbc47d466eb9fa1da0290dd7080319806
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-10-01 11:48:04 -0400

    Initial setup for training container

---
+++ src/train/docker-shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Create the network if we don't have it yet
+docker network inspect train-primera-network >/dev/null 2>&1 || docker network create train-primera-network
+
+# Build the image based on the Dockerfile
+docker build -t train-primera --platform=linux/arm64/v8 -f Dockerfile .
+
+# Run All Containers
+docker-compose run --rm --service-ports train-primera
