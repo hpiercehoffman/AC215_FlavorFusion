a6df6b0fceb3094b7cfba3e9c36f5d41f79c0c92
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-10-02 21:59:40 -0400

    try updating platform

--- src/train/docker-shell.sh
+++ src/train/docker-shell.sh
@@ -6,7 +6,7 @@ set -e
 docker network inspect train-primera-network >/dev/null 2>&1 || docker network create train-primera-network
 
 # Build the image based on the Dockerfile
-docker build -t train-primera --platform=linux/arm64/v8 -f Dockerfile .
+docker build -t train-primera --platform=linux/amd64 -f Dockerfile .
 
 # Run All Containers
 docker-compose run --rm --service-ports train-primera
