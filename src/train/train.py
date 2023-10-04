4447aa3d2b649561e959881a76c79ed9d8275cf5
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-10-03 21:25:11 -0400

    save total limit

--- src/train/train.py
+++ src/train/train.py
@@ -332,7 +332,6 @@ def main(args):
         output_dir=os.path.join(args.input_dir, "results"),
         evaluation_strategy="epoch",
         save_strategy="epoch",
-        save_total_limit = 5,
         report_to="wandb" if args.wandb else None,
         learning_rate=args.lr,
         per_device_train_batch_size=args.batch_size,
