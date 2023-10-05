ecff97be99c995ba8e96c0a578e6695ca44c0b70
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-10-04 22:02:23 -0400

    fix package trainer script; training screenshot

--- src/train/package-trainer.sh
+++ src/train/package-trainer.sh
@@ -1,4 +1,4 @@
 rm -f trainer.tar trainer.tar.gz
 tar cvf trainer.tar package
 gzip trainer.tar
-gsutil cp trainer.tar.gz $GCS_BUCKET_URI/train-primera.tar.gz
+gsutil cp trainer.tar.gz gs://primera-training/train-primera.tar.gz
