6895a38e48da75719e6327896582dd13bb5c33fd
Varun Ullanat <varappu.ram@gmail.com>
2023-10-20 03:43:40 -0400

    Added pruning and quantization functionalities during training

--- src/train_serverless/train_serverless.py
+++ src/train_serverless/train_serverless.py
@@ -11,9 +11,6 @@ import random
 import os
 import wandb
 import argparse
-import shutil
-
-from google.cloud import storage
 
 from datasets import load_dataset, load_metric
 from transformers import (
@@ -28,17 +25,19 @@ from transformers import (
     set_seed,
 )
 
-# Some functions in this script are adapted from the following example:
-# https://gist.github.com/JohnGiorgi/8c7dcabd3ee8a362b9174c5d145029ab
 
 def download_data(local_folder):
-    """Download preprocessed LSARS data from GCS bucket.
-    """
-    GCS_BUCKET_NAME = os.environ["GCS_DATA_BUCKET"]
+    from google.cloud import translate_v2 as translate
+    from google.cloud import storage
+    GCS_BUCKET_NAME = os.environ["GCS_BUCKET_NAME"]
     
     bucket_name = GCS_BUCKET_NAME
     print("Downloading data from " + str(bucket_name))
     
+    # Clear existing dataset folders so we get a clean copy
+    shutil.rmtree(local_folder, ignore_errors=True, onerror=None)
+    os.makedirs(local_folder, exist_ok=True)
+
     # Initiate storage client and download data
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -52,13 +51,23 @@ def download_data(local_folder):
 
 
 def get_global_attention_mask(input_ids, token_ids):
-    """Create global attention mask for Longformer Encoder Decoder.
+    """Returns a corresponding global attention mask for `input_ids`, which is 1 for any tokens in
+    `token_ids` (indicating the model should attend to those tokens) and 0 elsewhere (indicating the
+    model should not attend to those tokens).
+    # Parameters
+    input_ids : `List[List[str]]`
+        The input ids that will be provided to a model during the forward pass.
+    token_ids : `List[List[str]]`
+        The token ids that should be globally attended to.
     """
     global_attention_mask = [[1 if token_id in token_ids else 0 for token_id in batch] for batch in input_ids]
     return global_attention_mask
 
+
 def split_docs(text: str, doc_sep_token: str):
-    """Split a string into multiple reviews based on separator token.
+    """Given `text`, a string which contains the input documents seperated by `doc_sep_token`,
+    returns a list of each individual documents. Ignores any documents that are empty.
+    order of documents in each example.
     """
     # It's possible to have a doc_sep_token at the very end of the string. Strip it here
     # so that we get the correct number of documents when we split on doc_sep_token.
@@ -67,16 +76,29 @@ def split_docs(text: str, doc_sep_token: str):
 
 
 def get_num_docs(text: str, doc_sep_token: str) -> int:
-    """Get number of reviews in a string with separator token."""
+    """Given `text`, a string which contains the input documents seperated by `doc_sep_token`,
+    returns the number of individual documents.
+    """
+    # See: https://stackoverflow.com/a/3393470
     return len(list(filter(bool, split_docs(text, doc_sep_token=doc_sep_token))))
 
 
-def truncate_multi_doc(text: str, doc_sep: str, doc_sep_token: str, max_length: int, tokenizer):
-    """Truncate reviews in a token-separated string so that the length of all reviews together
-    does not exceed max_length.
+def truncate_multi_doc(
+    text: str,
+    doc_sep: str,
+    doc_sep_token: str,
+    max_length: int,
+    tokenizer):
+    
+    """Given some `text`, which is assumed to be multiple documents joined by `doc_sep_token`,
+    truncates each document (using `tokenizer`) so that the length of the concatenation of all
+    documents does not exceed max_length. See https://aclanthology.org/2021.naacl-main.380/ and
+    https://arxiv.org/abs/2110.08499 for more details. If `num_docs` is provided, the truncation
+    is done as if there are `num_docs` number of input documents. This is useful to control
+    for truncation when applying pertubations (e.g. additiion and deletion).
     """
     input_docs = split_docs(text, doc_sep_token=doc_sep)
-    # Determine number of reviews from the input text
+    # If num_docs is not provided, determine it from the input text
     num_docs = get_num_docs(text, doc_sep_token=doc_sep)
     # -2 to make room for the special tokens, -(len(docs) - 1) to make room for the doc sep tokens.
     max_doc_length = (max_length - 2 - (num_docs - 1)) // num_docs
@@ -89,9 +111,6 @@ def truncate_multi_doc(text: str, doc_sep: str, doc_sep_token: str, max_length: 
     return f" {doc_sep_token} ".join(truncated_docs)
 
 def sample_reviews(examples, max_docs_per_review=5, k_top_longest=20):
-    """Perform data augmentation by sampling the k longest reviews in a given data point,
-    then dividing into max_docs number of new data points.
-    """
     text_column = 'review_str'
     summary_column = 'summary'
     
@@ -113,57 +132,64 @@ def sample_reviews(examples, max_docs_per_review=5, k_top_longest=20):
 
 def preprocess_function(examples, tokenizer, text_column, summary_column, max_source_length, max_target_length, 
                         padding="max_length", ignore_pad_token_for_loss=True, prefix=""):
-    """Data preprocessing function which is called for all data points in the 
-    training and test sets. This function performs truncation and data augmentation
-    based on input parameters, then tokenizes, pads, and generates a global attention mask
-    so the data is ready to be used in the PRIMERA model.
-    """
-    # Create lists of inputs and targets from data
+    # remove pairs where at least one record is None
+    
     inputs, targets = [], []
     for i in range(len(examples[text_column])):
         if examples[text_column][i] and examples[summary_column][i]:
             inputs.append(examples[text_column][i])
             targets.append(examples[summary_column][i])
+
     inputs = [prefix + inp for inp in inputs]
 
-    # Truncation based on parameters
-    inputs = [truncate_multi_doc(
-                text, doc_sep="|||||",
-                doc_sep_token=tokenizer.additional_special_tokens[0],
-                max_length=max_source_length, tokenizer=tokenizer)
-            for text in inputs]
+    inputs = [
+        truncate_multi_doc(
+            text,
+            doc_sep="|||||",
+            doc_sep_token=tokenizer.additional_special_tokens[0],
+            max_length=max_source_length,
+            tokenizer=tokenizer,
+        )
+        for text in inputs
+    ]
 
-    # Tokenize inputs and targets
     model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
+
+    # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
         labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
 
-    # Convert pad tokens to -100 so the loss function ignores padding
+    # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+    # padding in the loss.
     if padding == "max_length" and ignore_pad_token_for_loss:
         labels["input_ids"] = [
-            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]]
+            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+        ]
 
     model_inputs["labels"] = labels["input_ids"]
 
-    # Add a global attention mask to model inputs
+    # Add a global attention mask to models inputs. We don't bother checking if the model will
+    # actually use it, as it will be ignored if not. For summarization, we place global attention
+    # on the document seperator token and the bos token (if it exists).
     global_attention_tokens = [tokenizer.bos_token, tokenizer.additional_special_tokens[0]]
     model_inputs["global_attention_mask"] = get_global_attention_mask(
         model_inputs.input_ids,
         token_ids=tokenizer.convert_tokens_to_ids(global_attention_tokens),
     )
+    #print(f"Using global attention on the following tokens: {global_attention_tokens}")
     return model_inputs
 
 def postprocess_text(preds, labels):
-    """Decode predictions after forward pass so we can do evaluation metrics."""
     preds = [pred.strip() for pred in preds]
     labels = [label.strip() for label in labels]
+
     # rougeLSum expects newline after each sentence
     preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
     labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
+
     return preds, labels
 
 def length_of_iterable_dataset(dataset):
-    """Helper function to get the length of a streaming dataset."""
     count=0
     for i in dataset:
         count+=1
@@ -171,25 +197,21 @@ def length_of_iterable_dataset(dataset):
 
 def main(args):
     
-    print("Now checking GPU availability")
-    print(torch.cuda.is_available())
-    
-    os.environ["GCS_DATA_BUCKET"] = args.gcs_bucket_name
-    
-    # You will be prompted for WandB API key if you haven't run "wandb auth"
+    # Make sure you have done 'wandb auth' 
     if args.wandb:
         os.environ["WANDB_PROJECT"]="FlavorFusion"
         os.environ["WANDB_LOG_MODEL"]="true"
         os.environ["WANDB_WATCH"]="false"
-        wandb.login(key=args.wandb_key)
         wandb.init(dir=args.input_dir)
+        
     
     # Load pre-trained model, tokenizer and config files
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-    config = AutoConfig.from_pretrained(args.model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
+    tokenizer = AutoTokenizer.from_pretrained('allenai/PRIMERA')
+    config = AutoConfig.from_pretrained('allenai/PRIMERA')
+    model = AutoModelForSeq2SeqLM.from_pretrained('allenai/PRIMERA')
     model.resize_token_embeddings(len(tokenizer))
     
+    # TODO: Have to check if this works
     if args.download:
         download_data(local_folder = args.input_dir)
         
@@ -200,7 +222,7 @@ def main(args):
     # Shuffle dataset and create train-test split
     raw_dataset = raw_dataset.shuffle(seed=42)
     if args.streaming:
-        TOTAL_SIZE = 5100
+        TOTAL_SIZE = 5000
         raw_dataset_test = raw_dataset.take(int(args.test_ratio*TOTAL_SIZE))
         raw_dataset_train = raw_dataset.skip(int(args.test_ratio*TOTAL_SIZE))
         column_names = ['review_str', 'summary', 'id']
@@ -305,9 +327,26 @@ def main(args):
         result = {k: round(v, 4) for k, v in result.items()}
         return result
     
+    
+    if args.quantize:
+        from optimum.intel import INCModelForSeq2SeqLM, INCSeq2SeqTrainer
+        from neural_compressor import QuantizationAwareTrainingConfig
+        
+        quantization_config = QuantizationAwareTrainingConfig()
+    if args.prune:
+        from optimum.intel import INCModelForSeq2SeqLM, INCSeq2SeqTrainer
+        from neural_compressor import WeightPruningConfig
+        
+        pruning_config = WeightPruningConfig(pruning_type="magnitude",
+                                             start_step=0,
+                                             end_step=args.num_train_epochs,
+                                             target_sparsity=0.2,
+                                             pruning_scope="local")
+    
+    
     # Generate training arguments 
     training_args = Seq2SeqTrainingArguments(
-        output_dir=os.path.join(args.input_dir, "results"),
+        output_dir="./results",
         evaluation_strategy="epoch",
         save_strategy="epoch",
         report_to="wandb" if args.wandb else None,
@@ -323,15 +362,29 @@ def main(args):
         fp16=True,
         predict_with_generate=True)
     
+    
     # Initialize main trainer 
-    trainer = Seq2SeqTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
-        tokenizer=tokenizer,
-        data_collator=data_collator,
-        compute_metrics=compute_metrics)
+    if (args.quantize or args.prune):
+        trainer = INCSeq2SeqTrainer(
+            model=model,
+            args=training_args,
+            quantization_config=quantization_config if args.quantize else None,
+            pruning_config=pruning_config if args.prune else None,
+            train_dataset=train_dataset,
+            eval_dataset=train_dataset,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+        )
+    else:
+        trainer = Seq2SeqTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=test_dataset,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics)
 
     # Train
     train_result = trainer.train()
@@ -357,23 +410,23 @@ if __name__ == "__main__":
     parser.add_argument('--download', action="store_true", help="Download processed LSARS data from GCS bucket")
     parser.add_argument('--wandb', action="store_true", default=False, help='Whether to use wandb for logging')
     parser.add_argument('--streaming', action="store_true", default=False, help='Whether to stream data')
-    parser.add_argument("--wandb_key", dest="wandb_key", default="16", type=str, help="WandB API Key")
-    parser.add_argument("--gcs_bucket_name", type=str, help='Path to GCS bucket to download data')
 
+    
     # Data processing args
     parser.add_argument('--test_ratio', type=float, default=0.05, help='Test ratio for splitting')
     parser.add_argument('--max_docs_per_review', type=int, default=5, help='Maximum number of reviews per data point')
     parser.add_argument('--k_top_longest', type=int, default=20, help='Keep only the k longest reviews for each data point')
     parser.add_argument('--num_processes', type=int, default=4, help='Number of processes used for multiprocessing the dataset')
     parser.add_argument('--max_source_length', type=int, default=4096, help='Maximum number of tokens for each set of reviews')
-    parser.add_argument('--max_target_length', type=int, default=1024, help='Maximum number of tokens for each summary in a review set')
+    parser.add_argument('--max_target_length', type=int, default=1024, help='Maximum number of tokens for each summary in areview set')
     parser.add_argument('--subset_dataset_to', type=int, default=None, help='Whether to subset dataset for debugging')
     
     # Training args
     parser.add_argument('--lr', type=float, default=2e-5, help='Learning rate for training')
     parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training')
-    parser.add_argument('--num_train_epochs', type=int, default=20, help='Total number of epochs for training')
-    parser.add_argument('--model_name', type=str,default='allenai/PRIMERA', help='Name of pretrained model to use')
+    parser.add_argument('--num_train_epochs', type=int, default=50, help='Total number of epochs for training')
+    parser.add_argument('--quantize', action="store_true", help='Whether to perform quantization on the model during the training')
+    parser.add_argument('--prune', action="store_true", help='Whether to perform pruning on the model during the training')
     
     args = parser.parse_args()
     
