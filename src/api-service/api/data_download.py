5f8cdb9f3125b66f90d94efe4fb616eb9acf42f1
Varun Ullanat <varappu.ram@gmail.com>
2023-11-19 17:01:48 -0500

    Fixed data download from gcloud

--- src/api-service/api/data_download.py
+++ src/api-service/api/data_download.py
@@ -12,26 +12,16 @@ def download_reviews():
     bucket_name = GCS_BUCKET_NAME
     print("Downloading data from " + str(bucket_name))
     
-    # Clear existing dataset folders so we get a clean copy
-    dataset_folder = "./"
-    #shutil.rmtree(dataset_folder, ignore_errors=True, onerror=None)
-    os.makedirs(dataset_folder, exist_ok=True)
-
     # Initiate storage client and download data
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
-    blobs = bucket.list_blobs(prefix="combined-data/")
-    for blob in blobs:
-        if not blob.name.endswith("combined-data/"):
-            print("Downloading data file:", blob.name)
-            filename = os.path.basename(blob.name)
-            local_file_path = os.path.join(dataset_folder, filename)
-            blob.download_to_filename(local_file_path)
-            if 'small' in local_file_path:
-                small_file_path = local_file_path
-            else:
-                large_file_path = local_file_path
-    return small_file_path, large_file_path
+    local_file_path = "../persistent/combined-data-combined-Massachusetts_small.csv"
+
+    blob = bucket.blob("combined-data/combined-data-combined_Massachusetts_small.csv")
+    blob.download_to_filename(local_file_path)
+     
+    small_file_path = local_file_path
+    return small_file_path, small_file_path
 
 
 if __name__ == "__main__":
