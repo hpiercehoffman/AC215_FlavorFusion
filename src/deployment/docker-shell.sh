e73f3a37b4ed602633bf8c2ad92fe36352de6b89
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-11-29 13:16:00 -0500

    Ansible for deploying and pushing docker images to GCR

--- src/deployment/docker-shell.sh
+++ src/deployment/docker-shell.sh
@@ -22,7 +22,7 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$SECRETS_DIR":/secrets \
 -v "$HOME/.ssh":/home/app/.ssh \
 -v "$BASE_DIR/../api-service":/api-service \
--v "$BASE_DIR/../frontend-simple":/frontend-react \
+-v "$BASE_DIR/../frontend-simple":/frontend-simple \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e USE_GKE_GCLOUD_AUTH_PLUGIN=True \
 -e GCP_PROJECT=$GCP_PROJECT \
