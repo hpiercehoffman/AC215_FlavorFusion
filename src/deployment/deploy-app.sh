88bb7c7585bcb45b4d0dff3c1bec023c44cb057b
Hannah Pierce-Hoffman <hrp2112@columbia.edu>
2023-12-07 14:15:33 -0500

    fix typo in deployment script

--- src/deployment/deploy-app.sh
+++ src/deployment/deploy-app.sh
@@ -1,2 +1,2 @@
 ansible-playbook deploy-docker-images.yml -i inventory.yml
-ansible-playbook deploy-cluster-short.yml -i inventory-prod.yml
+ansible-playbook deploy-cluster-short.yml -i inventory.yml
